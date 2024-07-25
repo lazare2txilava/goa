ca73f276e09fdda8dc96a3dfecb823f7ee97011f
lazare2txilava <1lazotxilo09@gmail.com>
2024-07-25 13:51:00 +0400

    first commit

---
+++ level03/homework/homework.py
@@ -0,0 +1,20 @@
+name="lazare"
+name1="lizi"
+name2="gio"
+name3="alepsi"
+name4="mariami"
+name5="dachi"
+
+lastname1="txilava"
+lastname2="somkhisvili"
+lastname3="basharuli"
+lastname4="nokarishvili"
+lastname5="grdzelo"
+
+print(name + lastname1)
+print(name1 + lastname2)
+print(name2 + lastname3)
+print(name2 + lastname2)
+print(name3 + lastname3)
+print(name4 + lastname4)
+print(name5 + lastname5)
