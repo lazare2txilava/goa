e941ed7685be67a75918468c2291dbc370144b2c
lazare2txilava <1lazotxilo09@gmail.com>
2024-08-07 21:53:43 +0400

    Add files via upload

---
+++ level8/level05/homework/level12/classwork.py
@@ -0,0 +1,10 @@
+input=int(input("enter your name"))
+
+if number == 30:
+    print("you won 1 dollar")
+
+ 
+    if ("costumer is under the age of 18 they will get discount")
+
+if ("he is a teenager he will get discount and if he is under the age of 19 he will get another discount")
+    
